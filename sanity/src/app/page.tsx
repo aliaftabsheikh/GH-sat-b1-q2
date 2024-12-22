8c07557979a523b71f5a3686263830937c69818c
Muhammad Bilal <61741931+Muhammad-Bilal90@users.noreply.github.com>
2024-12-23 01:56:17 +0500

    class work

--- sanity/src/app/page.tsx
+++ sanity/src/app/page.tsx
@@ -1,7 +1,10 @@
 import { client } from "@/sanity/lib/client";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
+
 
 export default async function Home() {
-  const query = `*[_type == "homePage"]{
+  const query = `*[_type == "homePage"][0]{
   heading,
   paragraph,
   buttonText,
@@ -16,6 +19,10 @@ console.log("Sanity Data: ", sanityData);
   return (
     <div className="">
       <h1 className="font-bold text-2xl">Sanity</h1>
+      <Image src={urlFor(sanityData.image).url()} alt="Sanity-Image" width={200} height={200} />
+      <h2 className="font-bold text-xl">{sanityData.heading}</h2>
+      <p>{sanityData.paragraph}</p>
+      <button className="border-2 border-solid border-white">{sanityData.buttonText}</button>
     </div>
   );
 }
